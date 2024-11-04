467cf228e453601407ac80046bd04092d64f01c3
Carlos Mazzarri <49962605+cjmazzarri@users.noreply.github.com>
2024-11-04 13:51:01 -0500

    Added example

---
+++ Ejemplo/Ejemplo/EjemploApp.swift
@@ -0,0 +1,17 @@
+//
+//  EjemploApp.swift
+//  Ejemplo
+//
+//  Created by Juan Carlos Hernandez on 4/11/24.
+//
+
+import SwiftUI
+
+@main
+struct EjemploApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Ejemplo/EjemploTests/EjemploTests.swift
@@ -0,0 +1,16 @@
+//
+//  EjemploTests.swift
+//  EjemploTests
+//
+//  Created by Juan Carlos Hernandez on 4/11/24.
+//
+
+import Testing
+
+struct EjemploTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
